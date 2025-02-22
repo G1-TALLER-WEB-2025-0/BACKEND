7817d6b86b7bc2d75157785ecf8fdee395e57d3c
Rainogo <106415585+angeloVillar@users.noreply.github.com>
2025-02-22 15:50:04 -0500

    correcciones a la estructura del proyecto
    refactor a todos los imports
    - añadi "com."
    - removi .domains
    
    refactor al group id del pom.xml

--- Plataforma-Educativa/Plataforma-Educativa/src/main/java/Plataforma_Educativa/repositories/assessmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Plataforma_Educativa.domains.repositories;
-
-import com.Plataforma_Educativa.domains.entity.assessment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface assessmentRepository extends JpaRepository<assessment, Long> {
-}
--- Plataforma-Educativa/Plataforma-Educativa/src/main/java/Plataforma_Educativa/repositories/videoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Plataforma_Educativa.domains.repositories;
-
-import com.Plataforma_Educativa.domains.entity.video;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface videoRepository extends JpaRepository<video, Long> {
-}
